fec1aee45fdb67e9c5a55c43d9b7301f04e14924
simonjstanford <simonjstanford@gmail.com>
2014-03-12 13:00:02 +0000

    Most recent version

--- XbmcWatchedFlagUpdater/GUI.cs
+++ XbmcWatchedFlagUpdater/GUI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,17 +21,61 @@ namespace XbmcWatchedFlagUpdater
         private void GUI_Load(object sender, EventArgs e)
         {
             //Retrieve previous settings
-            textBoxServerIP.Text = Properties.Settings.Default.ServerIP;
-            textBoxUsername.Text = Properties.Settings.Default.Username;
-            textBoxPassword.Text = Properties.Settings.Default.Password;
+            textBoxServerIP.Text = Properties.Settings.Default.ServerIP;         
             textBoxImportPathRemote.Text = Properties.Settings.Default.ImportPathRemote;
             textBoxExportPathRemote.Text = Properties.Settings.Default.ExportPathRemote;
+
             textBoxLocalDBPath.Text = Properties.Settings.Default.LocalDBFile;
+            textBoxImportPathLocal.Text = Properties.Settings.Default.ImportPathRemote;
+            textBoxExportPathLocal.Text = Properties.Settings.Default.ExportPathRemote;
+            
+            //load defaults
             if (textBoxLocalDBPath.Text.Length < 1)
                 textBoxLocalDBPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\XBMC\userdata\Database\MyVideos75.db";
+            
             textBoxDatabaseName.Text = Properties.Settings.Default.DatabaseName;
             if (textBoxDatabaseName.Text.Length < 1)
                 textBoxDatabaseName.Text = "myvideos75";
+
+            textBoxUsername.Text = Properties.Settings.Default.Username;
+            if (textBoxUsername.Text.Length < 1)
+                textBoxUsername.Text = "xbmc";
+
+            textBoxPassword.Text = Properties.Settings.Default.Password;
+            if (textBoxPassword.Text.Length < 1)
+                textBoxPassword.Text = "xbmc";
+        }
+
+        private void disableButtons()
+        {
+            toolStripProgressBar1.Visible = true;
+
+            toolStripStatusLabel1.Visible = false;
+
+            buttonSelectFileImportRemote.Enabled = false;
+            buttonImportRemote.Enabled = false;
+            buttonSelectFileExportRemote.Enabled = false;
+            buttonExportRemote.Enabled = false;
+
+            buttonImportSelectFileLocal.Enabled = false;
+            buttonImportLocal.Enabled = false;
+            buttonExportSelectFileLocal.Enabled = false;
+            buttonExportLocal.Enabled = false;
+        }
+
+        private void enableButtons()
+        {
+            toolStripProgressBar1.Visible = false;
+
+            buttonSelectFileImportRemote.Enabled = true;
+            buttonImportRemote.Enabled = true;
+            buttonSelectFileExportRemote.Enabled = true;
+            buttonExportRemote.Enabled = true;
+
+            buttonImportSelectFileLocal.Enabled = true;
+            buttonImportLocal.Enabled = true;
+            buttonExportSelectFileLocal.Enabled = true;
+            buttonExportLocal.Enabled = true;
         }
 
         #region Remote Buttons and Background Workers
@@ -71,10 +115,15 @@ namespace XbmcWatchedFlagUpdater
                 //read files
                 List<XbmcFile> files = XMLConnector.ReadFiles(textBoxImportPathRemote.Text);
 
-                //run import in different thread
-                XbmcMySqlConnector sql = new XbmcMySqlConnector(textBoxServerIP.Text, textBoxUsername.Text, textBoxPassword.Text, files);
-                toolStripProgressBar1.Style = ProgressBarStyle.Marquee; //turn on the progress bar
-                backgroundWorkerImportRemote.RunWorkerAsync(sql);
+                if (files != null)
+                {
+                    disableButtons();
+
+                    //run import in different thread
+                    XbmcMySqlConnector sql = new XbmcMySqlConnector(textBoxServerIP.Text, textBoxUsername.Text, textBoxPassword.Text, files);
+                    toolStripProgressBar1.Style = ProgressBarStyle.Marquee; //turn on the progress bar
+                    backgroundWorkerImportRemote.RunWorkerAsync(sql);
+                }
             }
             else
             {
@@ -115,6 +164,7 @@ namespace XbmcWatchedFlagUpdater
                 Properties.Settings.Default.Save();
 
                 //run export in different thread
+                disableButtons();
                 XbmcMySqlConnector sql = new XbmcMySqlConnector(textBoxServerIP.Text, textBoxUsername.Text, textBoxPassword.Text);
                 toolStripProgressBar1.Style = ProgressBarStyle.Marquee; //turn on the progress bar
                 backgroundWorkerExportRemote.RunWorkerAsync(sql);
@@ -129,7 +179,8 @@ namespace XbmcWatchedFlagUpdater
         {
             //cast the connector then execute import command
             XbmcMySqlConnector sql = (XbmcMySqlConnector)e.Argument;
-            MessageBox.Show("Import complete. " + sql.ImportWatchedFlags().ToString() + " rows updated.");
+            e.Result = sql.ImportWatchedFlags().ToString();
+            //MessageBox.Show("Import complete. " + sql.ImportWatchedFlags().ToString() + " rows updated.");
         }
 
         private void backgroundWorkerExportRemote_DoWork(object sender, DoWorkEventArgs e)
@@ -139,21 +190,26 @@ namespace XbmcWatchedFlagUpdater
             List<XbmcFile> files = sql.ExportWatchedFlags();
             files.Sort();
             XMLConnector.WriteFiles(textBoxExportPathRemote.Text, files);
-            MessageBox.Show("Export complete. " + files.Count + " files found.");
+            e.Result = files.Count;
+            //MessageBox.Show("Export complete. " + files.Count + " files found.");
         }
 
         private void backgroundWorkerImportRemote_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            enableButtons();
             toolStripProgressBar1.Style = ProgressBarStyle.Blocks; //disable progress bar when finished
+            toolStripStatusLabel1.Text = "Import complete. " + (string)e.Result + " rows updated.";
+            toolStripStatusLabel1.Visible = true;
         }
 
         private void backgroundWorkerExportRemote_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            enableButtons();
             toolStripProgressBar1.Style = ProgressBarStyle.Blocks;  //disable progress bar when finished
-        }
-
+            toolStripStatusLabel1.Text = "Export complete. " + (int)e.Result + " files found.";
+            toolStripStatusLabel1.Visible = true;
+        }     
         #endregion
-
 
         #region Local Buttons and Background Workers
 
@@ -207,10 +263,15 @@ namespace XbmcWatchedFlagUpdater
                 //read files
                 List<XbmcFile> files = XMLConnector.ReadFiles(textBoxImportPathLocal.Text);
 
-                //run import in different thread
-                XbmcSqlLiteConnector sql = new XbmcSqlLiteConnector(textBoxLocalDBPath.Text, files);
-                toolStripProgressBar1.Style = ProgressBarStyle.Marquee; //turn on the progress bar
-                backgroundWorkerImportLocal.RunWorkerAsync(sql);
+                if (files != null)
+                {
+                    disableButtons();
+
+                    //run import in different thread
+                    XbmcSqlLiteConnector sql = new XbmcSqlLiteConnector(textBoxLocalDBPath.Text, files);
+                    toolStripProgressBar1.Style = ProgressBarStyle.Marquee; //turn on the progress bar
+                    backgroundWorkerImportLocal.RunWorkerAsync(sql);
+                }
             }
             else
             {
@@ -246,6 +307,8 @@ namespace XbmcWatchedFlagUpdater
                 Properties.Settings.Default.ExportPathLocal = textBoxExportPathLocal.Text;
                 Properties.Settings.Default.Save();
 
+                disableButtons();
+
                 //run export in different thread
                 XbmcSqlLiteConnector sql = new XbmcSqlLiteConnector(textBoxLocalDBPath.Text);
                 toolStripProgressBar1.Style = ProgressBarStyle.Marquee; //turn on the progress bar
@@ -261,12 +324,16 @@ namespace XbmcWatchedFlagUpdater
         {
             //cast the connector then execute import command
             XbmcSqlLiteConnector sql = (XbmcSqlLiteConnector)e.Argument;
-            MessageBox.Show("Import complete. " + sql.ImportWatchedFlags().ToString() + " rows updated.");
+            e.Result = sql.ImportWatchedFlags().ToString();
+            //MessageBox.Show("Import complete. " + sql.ImportWatchedFlags().ToString() + " rows updated.");
         }
 
         private void backgroundWorkerImportLocal_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            enableButtons();
             toolStripProgressBar1.Style = ProgressBarStyle.Blocks; //disable progress bar when finished
+            toolStripStatusLabel1.Text = "Import complete. " + (string)e.Result + " rows updated.";
+            toolStripStatusLabel1.Visible = true;
         }
 
         private void backgroundWorkerExportLocal_DoWork(object sender, DoWorkEventArgs e)
@@ -275,12 +342,16 @@ namespace XbmcWatchedFlagUpdater
             XbmcSqlLiteConnector sql = (XbmcSqlLiteConnector)e.Argument;
             List<XbmcFile> files = sql.ExportWatchedFlags();
             XMLConnector.WriteFiles(textBoxExportPathLocal.Text, files);
-            MessageBox.Show("Export complete. " + files.Count + " files found.");
+            e.Result = files.Count;
+            //MessageBox.Show("Export complete. " + files.Count + " files found.");
         }
 
         private void backgroundWorkerExportLocal_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            enableButtons();
             toolStripProgressBar1.Style = ProgressBarStyle.Blocks; //disable progress bar when finished
+            toolStripStatusLabel1.Text = "Export complete. " + (int)e.Result + " files found.";
+            toolStripStatusLabel1.Visible = true;
         }
         #endregion
     }
