e37dbfb0755df29e81ee6e9755adf9c539a6853c
simonjstanford <simonjstanford@gmail.com>
2014-03-12 13:00:58 +0000

    most recent version

--- XbmcWatchedFlagUpdater/GUI.designer.cs
+++ XbmcWatchedFlagUpdater/GUI.designer.cs
@@ -1,4 +1,4 @@
-﻿namespace XbmcWatchedFlagUpdater
+namespace XbmcWatchedFlagUpdater
 {
     partial class GUI
     {
@@ -70,6 +70,7 @@
             this.textBoxLocalDBPath = new System.Windows.Forms.TextBox();
             this.backgroundWorkerImportLocal = new System.ComponentModel.BackgroundWorker();
             this.backgroundWorkerExportLocal = new System.ComponentModel.BackgroundWorker();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -94,10 +95,11 @@
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripProgressBar1});
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 372);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(319, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(346, 22);
             this.statusStrip1.TabIndex = 9;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -105,6 +107,7 @@
             // 
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
             this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Visible = false;
             // 
             // backgroundWorkerExportRemote
             // 
@@ -118,7 +121,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(299, 353);
+            this.tabControl1.Size = new System.Drawing.Size(327, 353);
             this.tabControl1.TabIndex = 10;
             // 
             // tabPage1
@@ -129,7 +132,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(291, 327);
+            this.tabPage1.Size = new System.Drawing.Size(319, 327);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Shared MySQL Library";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -141,7 +144,7 @@
             this.groupBoxExport.Controls.Add(this.buttonExportRemote);
             this.groupBoxExport.Location = new System.Drawing.Point(6, 239);
             this.groupBoxExport.Name = "groupBoxExport";
-            this.groupBoxExport.Size = new System.Drawing.Size(277, 80);
+            this.groupBoxExport.Size = new System.Drawing.Size(306, 80);
             this.groupBoxExport.TabIndex = 13;
             this.groupBoxExport.TabStop = false;
             this.groupBoxExport.Text = "Export";
@@ -150,12 +153,12 @@
             // 
             this.textBoxExportPathRemote.Location = new System.Drawing.Point(6, 19);
             this.textBoxExportPathRemote.Name = "textBoxExportPathRemote";
-            this.textBoxExportPathRemote.Size = new System.Drawing.Size(260, 20);
+            this.textBoxExportPathRemote.Size = new System.Drawing.Size(291, 20);
             this.textBoxExportPathRemote.TabIndex = 8;
             // 
             // buttonSelectFileExportRemote
             // 
-            this.buttonSelectFileExportRemote.Location = new System.Drawing.Point(110, 45);
+            this.buttonSelectFileExportRemote.Location = new System.Drawing.Point(141, 51);
             this.buttonSelectFileExportRemote.Name = "buttonSelectFileExportRemote";
             this.buttonSelectFileExportRemote.Size = new System.Drawing.Size(75, 23);
             this.buttonSelectFileExportRemote.TabIndex = 5;
@@ -165,7 +168,7 @@
             // 
             // buttonExportRemote
             // 
-            this.buttonExportRemote.Location = new System.Drawing.Point(191, 45);
+            this.buttonExportRemote.Location = new System.Drawing.Point(222, 51);
             this.buttonExportRemote.Name = "buttonExportRemote";
             this.buttonExportRemote.Size = new System.Drawing.Size(75, 23);
             this.buttonExportRemote.TabIndex = 6;
@@ -181,14 +184,14 @@
             this.groupBoxImport.Controls.Add(this.buttonImportRemote);
             this.groupBoxImport.Location = new System.Drawing.Point(6, 136);
             this.groupBoxImport.Name = "groupBoxImport";
-            this.groupBoxImport.Size = new System.Drawing.Size(277, 97);
+            this.groupBoxImport.Size = new System.Drawing.Size(306, 97);
             this.groupBoxImport.TabIndex = 12;
             this.groupBoxImport.TabStop = false;
             this.groupBoxImport.Text = "Import";
             // 
             // buttonSelectFileImportRemote
             // 
-            this.buttonSelectFileImportRemote.Location = new System.Drawing.Point(113, 64);
+            this.buttonSelectFileImportRemote.Location = new System.Drawing.Point(141, 64);
             this.buttonSelectFileImportRemote.Name = "buttonSelectFileImportRemote";
             this.buttonSelectFileImportRemote.Size = new System.Drawing.Size(75, 23);
             this.buttonSelectFileImportRemote.TabIndex = 3;
@@ -209,12 +212,12 @@
             // 
             this.textBoxImportPathRemote.Location = new System.Drawing.Point(6, 38);
             this.textBoxImportPathRemote.Name = "textBoxImportPathRemote";
-            this.textBoxImportPathRemote.Size = new System.Drawing.Size(263, 20);
+            this.textBoxImportPathRemote.Size = new System.Drawing.Size(294, 20);
             this.textBoxImportPathRemote.TabIndex = 7;
             // 
             // buttonImportRemote
             // 
-            this.buttonImportRemote.Location = new System.Drawing.Point(194, 64);
+            this.buttonImportRemote.Location = new System.Drawing.Point(222, 64);
             this.buttonImportRemote.Name = "buttonImportRemote";
             this.buttonImportRemote.Size = new System.Drawing.Size(75, 23);
             this.buttonImportRemote.TabIndex = 4;
@@ -234,7 +237,7 @@
             this.groupBoxConnection.Controls.Add(this.textBoxUsername);
             this.groupBoxConnection.Location = new System.Drawing.Point(6, 6);
             this.groupBoxConnection.Name = "groupBoxConnection";
-            this.groupBoxConnection.Size = new System.Drawing.Size(278, 124);
+            this.groupBoxConnection.Size = new System.Drawing.Size(307, 124);
             this.groupBoxConnection.TabIndex = 11;
             this.groupBoxConnection.TabStop = false;
             this.groupBoxConnection.Text = "MySQL Server Connection";
@@ -252,7 +255,7 @@
             // 
             this.textBoxDatabaseName.Location = new System.Drawing.Point(80, 94);
             this.textBoxDatabaseName.Name = "textBoxDatabaseName";
-            this.textBoxDatabaseName.Size = new System.Drawing.Size(189, 20);
+            this.textBoxDatabaseName.Size = new System.Drawing.Size(220, 20);
             this.textBoxDatabaseName.TabIndex = 7;
             // 
             // labelServerIP
@@ -268,7 +271,7 @@
             // 
             this.textBoxPassword.Location = new System.Drawing.Point(80, 68);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(189, 20);
+            this.textBoxPassword.Size = new System.Drawing.Size(220, 20);
             this.textBoxPassword.TabIndex = 2;
             // 
             // labelPassword
@@ -284,7 +287,7 @@
             // 
             this.textBoxServerIP.Location = new System.Drawing.Point(80, 19);
             this.textBoxServerIP.Name = "textBoxServerIP";
-            this.textBoxServerIP.Size = new System.Drawing.Size(189, 20);
+            this.textBoxServerIP.Size = new System.Drawing.Size(220, 20);
             this.textBoxServerIP.TabIndex = 0;
             // 
             // labelUser
@@ -300,7 +303,7 @@
             // 
             this.textBoxUsername.Location = new System.Drawing.Point(80, 42);
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(189, 20);
+            this.textBoxUsername.Size = new System.Drawing.Size(220, 20);
             this.textBoxUsername.TabIndex = 1;
             // 
             // tabPage2
@@ -311,7 +314,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(291, 327);
+            this.tabPage2.Size = new System.Drawing.Size(319, 327);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Local SQLite Library";
             // 
@@ -322,7 +325,7 @@
             this.groupBox1.Controls.Add(this.buttonExportLocal);
             this.groupBox1.Location = new System.Drawing.Point(6, 217);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(277, 80);
+            this.groupBox1.Size = new System.Drawing.Size(307, 104);
             this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Export";
@@ -331,12 +334,12 @@
             // 
             this.textBoxExportPathLocal.Location = new System.Drawing.Point(6, 19);
             this.textBoxExportPathLocal.Name = "textBoxExportPathLocal";
-            this.textBoxExportPathLocal.Size = new System.Drawing.Size(260, 20);
+            this.textBoxExportPathLocal.Size = new System.Drawing.Size(295, 20);
             this.textBoxExportPathLocal.TabIndex = 8;
             // 
             // buttonExportSelectFileLocal
             // 
-            this.buttonExportSelectFileLocal.Location = new System.Drawing.Point(110, 45);
+            this.buttonExportSelectFileLocal.Location = new System.Drawing.Point(145, 45);
             this.buttonExportSelectFileLocal.Name = "buttonExportSelectFileLocal";
             this.buttonExportSelectFileLocal.Size = new System.Drawing.Size(75, 23);
             this.buttonExportSelectFileLocal.TabIndex = 5;
@@ -346,7 +349,7 @@
             // 
             // buttonExportLocal
             // 
-            this.buttonExportLocal.Location = new System.Drawing.Point(191, 45);
+            this.buttonExportLocal.Location = new System.Drawing.Point(226, 45);
             this.buttonExportLocal.Name = "buttonExportLocal";
             this.buttonExportLocal.Size = new System.Drawing.Size(75, 23);
             this.buttonExportLocal.TabIndex = 6;
@@ -362,14 +365,14 @@
             this.groupBox2.Controls.Add(this.buttonImportLocal);
             this.groupBox2.Location = new System.Drawing.Point(6, 114);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(277, 97);
+            this.groupBox2.Size = new System.Drawing.Size(307, 97);
             this.groupBox2.TabIndex = 15;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Import";
             // 
             // buttonImportSelectFileLocal
             // 
-            this.buttonImportSelectFileLocal.Location = new System.Drawing.Point(113, 64);
+            this.buttonImportSelectFileLocal.Location = new System.Drawing.Point(145, 64);
             this.buttonImportSelectFileLocal.Name = "buttonImportSelectFileLocal";
             this.buttonImportSelectFileLocal.Size = new System.Drawing.Size(75, 23);
             this.buttonImportSelectFileLocal.TabIndex = 3;
@@ -390,12 +393,12 @@
             // 
             this.textBoxImportPathLocal.Location = new System.Drawing.Point(6, 38);
             this.textBoxImportPathLocal.Name = "textBoxImportPathLocal";
-            this.textBoxImportPathLocal.Size = new System.Drawing.Size(263, 20);
+            this.textBoxImportPathLocal.Size = new System.Drawing.Size(298, 20);
             this.textBoxImportPathLocal.TabIndex = 7;
             // 
             // buttonImportLocal
             // 
-            this.buttonImportLocal.Location = new System.Drawing.Point(194, 64);
+            this.buttonImportLocal.Location = new System.Drawing.Point(226, 64);
             this.buttonImportLocal.Name = "buttonImportLocal";
             this.buttonImportLocal.Size = new System.Drawing.Size(75, 23);
             this.buttonImportLocal.TabIndex = 4;
@@ -410,14 +413,14 @@
             this.groupBox3.Controls.Add(this.textBoxLocalDBPath);
             this.groupBox3.Location = new System.Drawing.Point(6, 6);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(278, 102);
+            this.groupBox3.Size = new System.Drawing.Size(307, 102);
             this.groupBox3.TabIndex = 14;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "XBMC Movie Database File Location";
             // 
             // buttonSelectLocalPath
             // 
-            this.buttonSelectLocalPath.Location = new System.Drawing.Point(194, 73);
+            this.buttonSelectLocalPath.Location = new System.Drawing.Point(226, 73);
             this.buttonSelectLocalPath.Name = "buttonSelectLocalPath";
             this.buttonSelectLocalPath.Size = new System.Drawing.Size(75, 23);
             this.buttonSelectLocalPath.TabIndex = 4;
@@ -438,7 +441,7 @@
             // 
             this.textBoxLocalDBPath.Location = new System.Drawing.Point(3, 32);
             this.textBoxLocalDBPath.Name = "textBoxLocalDBPath";
-            this.textBoxLocalDBPath.Size = new System.Drawing.Size(266, 20);
+            this.textBoxLocalDBPath.Size = new System.Drawing.Size(301, 20);
             this.textBoxLocalDBPath.TabIndex = 0;
             // 
             // backgroundWorkerImportLocal
@@ -451,17 +454,24 @@
             this.backgroundWorkerExportLocal.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerExportLocal_DoWork);
             this.backgroundWorkerExportLocal.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerExportLocal_RunWorkerCompleted);
             // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Visible = false;
+            // 
             // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 394);
+            this.ClientSize = new System.Drawing.Size(346, 394);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "GUI";
-            this.Text = "XBMC Watched Flag Helper 2.2";
+            this.Text = "XBMC Watched Flag Helper 2.9";
             this.Load += new System.EventHandler(this.GUI_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
@@ -529,6 +539,7 @@
         private System.ComponentModel.BackgroundWorker backgroundWorkerExportLocal;
         private System.Windows.Forms.Label labelDatabase;
         private System.Windows.Forms.TextBox textBoxDatabaseName;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
